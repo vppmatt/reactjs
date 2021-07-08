ff554980df63f253921e9be8610ce01360b2e22b
Matt Thornfield <matt@capabilitytm.com>
2021-07-08 10:45:23 +0100

    0103 fix

--- End of section workspaces/0103 - introducing components/hello-world/src/components/Greeting/Greeting.js
+++ End of section workspaces/0103 - introducing components/hello-world/src/components/Greeting/Greeting.js
@@ -1,11 +1,11 @@
 import React from 'react'; //This line is no longer needed
 import './Greeting.css';
 
-const Greeting = (props) => {
+const Greeting = ({name,age}) => {
 
     return (
         <div>
-            <p className="greeting_text"> Hello {props.name}. You are {props.age} years old. </p>
+            <p className="greeting_text"> Hello {name}. You are {age} years old. </p>
         </div>
     );
 }

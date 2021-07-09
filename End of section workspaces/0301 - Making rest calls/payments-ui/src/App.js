dad6591b19ec99fd945f684dd7d29c0104d77be7
Matt Thornfield <matt@capabilitytm.com>
2021-07-09 15:20:44 +0100

    0301

---
+++ End of section workspaces/0301 - Making rest calls/payments-ui/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import PageHeader from "./components/pageHeader/PageHeader";
+import Search from "./components/Search/Search";
+import Transactions from "./components/Transactions/Transactions";
+
+function App() {
+    return (
+        <div>
+            <PageHeader/>
+            <Search/>
+            <Transactions/>
+        </div>
+    );
+}
+
+export default App;
